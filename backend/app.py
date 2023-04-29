bb4a2a0239215519755343e57219427aa1bbc8e9
harrisonchin2523 <harrisonchin2523@gmail.com>
2023-04-29 11:53:01 -0400

    moving files to this repo

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,104 @@
 import json
 import os
+import re
+import math
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+from dotenv import load_dotenv
+import preprocessing
+import text_mining
 
-# ROOT_PATH for linking with all your files. 
+load_dotenv()
+nltk.download("punkt")
+
+# ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ["ROOT_PATH"] = os.path.abspath(os.path.join("..", os.curdir))
 
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+MYSQL_USER_PASSWORD = os.getenv("MYSQL_USER_PASSWORD")
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "kardashiandb"
+MYSQL_DATABASE = "playlistsdb"
 
-mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(
+    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE
+)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
+# albert xiao is so hot
 mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
 CORS(app)
 
-# Sample search, the LIKE operator in this case is hard-coded, 
-# but if you decide to use SQLAlchemy ORM framework, 
-# there's a much better and cleaner way to do this
-def sql_search(episode):
-    query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
-    data = mysql_engine.query_selector(query_sql)
-    return json.dumps([dict(zip(keys,i)) for i in data])
+preprocessing.preprocess()
+text_mining.init()
 
-@app.route("/")
-def home():
-    return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
-def episodes_search():
-    text = request.args.get("title")
-    return sql_search(text)
+def accumulate_dot_scores(query_word_counts, inv_idx, idf):
+    doc_scores = {}
+    for i, qi in query_word_counts.items():
+        if i in idf:
+            for j, dij in inv_idx[i]:
+                doc_scores[j] = doc_scores.get(j, 0) + idf[i] * qi * idf[i] * dij
 
-# app.run(debug=True)
+    return doc_scores
+
+
+def index_search(query, inv_idx, idf, doc_norms):
+    """
+    Search the collection of documents for the given query.
+
+    Returns
+    =======
+    results: sorted tuple list (score, pid)
+    """
+    results = []
+
+    query_tokens = preprocessing.tokenize(query)
+
+    query_word_counts = nltk.FreqDist(query_tokens)
+
+    query_norm = 0
+    for i, tf in query_word_counts.items():
+        if i in idf:
+            query_norm += (tf * idf[i]) ** 2
+    query_norm = np.sqrt(query_norm)
+
+    dot_scores = accumulate_dot_scores(query_word_counts, inv_idx, idf)
+    for doc, score in dot_scores.items():
+        cossim = score / (query_norm * doc_norms[doc])
+        results.append((cossim, doc))
+
+    results.sort(reverse=True, key=lambda x: x[0])
+    return results
+
+
+@app.route("/search")
+def search():
+    query = request.args.get("title")
+    k = 50  # Number of playlists to examine
+    top_playlists = index_search(
+        query, preprocessing.inv_idx, preprocessing.idf, preprocessing.doc_norms
+    )[:k]
+    song_scores = {}
+    for score, pid in top_playlists:
+        for track in preprocessing.playlists[pid]["tracks"]:
+            song = track["track_name"]
+            if song not in song_scores:
+                song_scores[song] = 0
+
+            song_scores[song] += score
+
+    ranked_songs = list(song_scores.items())
+    ranked_songs.sort(key=lambda x: x[1], reverse=True)
+    print(ranked_songs[:50])
+    return ranked_songs[:15]
+    # app.run(debug=True)
---
+++ frontend/src/App.tsx
@@ -0,0 +1,83 @@
+import React, { MouseEventHandler, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  // useEffect(() => {
+  //   fetch("http://localhost:5050/start");
+  // }, []);
+
+  return (
+    <>
+      <div className="parallax-wrap">
+        <div className="background-grid"></div>
+      </div>
+      <main>
+        <div className="top-text">
+          <div className="title">
+            <h1>
+              <span style={{ color: "deepskyblue" }}>Sound</span>Scout
+            </h1>
+          </div>
+          <div className="input-box" onClick={sendFocus}>
+            <img src="mag.png" onClick={search} />
+            <input placeholder="Search for a playlist" id="filter-text-val" />
+          </div>
+        </div>
+        <div id="answer-box"></div>
+      </main>
+      <footer>
+        Created by Harrison Chin, Willy Jiang, Joshua Guo, Eric Huang, Alex
+        Levinson
+      </footer>
+    </>
+  );
+}
+
+function answerBoxTemplate(title: string, titleDesc: string) {
+  return `<div class='song-title'>${title}</div>`;
+}
+
+const sendFocus: MouseEventHandler<HTMLDivElement> = (e) => {
+  (document.getElementById("filter-text-val") as HTMLInputElement).focus();
+};
+
+const search: MouseEventHandler<HTMLImageElement> = (e) => {
+  (document.getElementById("answer-box") as HTMLDivElement).innerHTML = "";
+  const box: HTMLElement = document.getElementById(
+    "answer-box"
+  ) as HTMLDivElement;
+  while (box.lastElementChild) {
+    box.removeChild(box.lastElementChild);
+  }
+  fetch(
+    "http://localhost:5050/search?" +
+    new URLSearchParams({
+      title: (document.getElementById("filter-text-val") as HTMLInputElement)
+        .value,
+    }).toString()
+  )
+    .then((response) => response.json())
+    .then((data) =>
+      data.forEach((row: string[]) => {
+        let tempDiv = document.createElement("div");
+        tempDiv.className = "answer";
+        tempDiv.innerHTML = answerBoxTemplate(row[0], row[3]);
+        (document.getElementById("answer-box") as HTMLDivElement).appendChild(
+          tempDiv
+        );
+      })
+    );
+};
+
+const parallax = (event: MouseEvent) => {
+  const x = (window.innerWidth - event.pageX * 1) / 90;
+  const y = (window.innerHeight - event.pageY * 1) / 90;
+
+  (
+    document.querySelector(".parallax-wrap .background-grid") as HTMLDivElement
+  ).style.transform = `translateX(${x}px) translateY(${y}px)`;
+};
+
+document.addEventListener("mousemove", parallax);
+
+export default App;
